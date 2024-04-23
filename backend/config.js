3a4f61fe525a4c9db09b7825f30e4fa91680c431
Thushh <thushithansivakumar@gmail.com>
2024-04-24 00:18:21 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://thushithan99:l123456789@books-store-mern.c6sxeb1.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
